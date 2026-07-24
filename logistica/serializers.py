0540e1542f5edfb4f60d50cbb79984e3ec879f15
kevin <kevingonzalesgr@gmail.com>
2026-07-23 22:42:44 -0600

    Actualizando

--- logistica/serializers.py
+++
@@ -1,2 +0,0 @@
-# serializers.py — pendiente de implementar
-# Aquí van los serializers de la API para el portal del cliente
--- logistica/urls.py
+++ logistica/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.home_redirect, name='home'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
     path('portal/', views.portal_cliente, name='portal_cliente'),
     path('mis-paquetes/', views.mis_paquetes, name='mis_paquetes'),
     path('rastreo/', views.rastreo, name='rastreo'),
